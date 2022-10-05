6e904c404e6d4b5014fdf3e48099809c4ddfe837
Maria Centurion <centurionmalena94@gmail.com>
2022-10-04 21:51:35 -0300

    Login nuevo

--- js/login_script.js
+++ js/login_script.js
@@ -1,33 +1,13 @@
-$(document).ready(function () {
-  var panelOne = $(".form-panel.two").height(),
-    panelTwo = $(".form-panel.two")[0].scrollHeight;
-
-  $(".form-panel.two")
-    .not(".form-panel.two.active")
-    .on("click", function (e) {
-      e.preventDefault();
-
-      $(".form-toggle").addClass("visible");
-      $(".form-panel.one").addClass("hidden");
-      $(".form-panel.two").addClass("active");
-      $(".form").animate(
-        {
-          height: panelTwo
-        },
-        200
-      );
-    });
-
-  $(".form-toggle").on("click", function (e) {
-    e.preventDefault();
-    $(this).removeClass("visible");
-    $(".form-panel.one").removeClass("hidden");
-    $(".form-panel.two").removeClass("active");
-    $(".form").animate(
-      {
-        height: panelOne
-      },
-      200
-    );
-  });
+$(".email-signup").hide();
+$("#signup-box-link").click(function(){
+  $(".email-login").fadeOut(100);
+  $(".email-signup").delay(100).fadeIn(100);
+  $("#login-box-link").removeClass("active");
+  $("#signup-box-link").addClass("active");
+});
+$("#login-box-link").click(function(){
+  $(".email-login").delay(100).fadeIn(100);;
+  $(".email-signup").fadeOut(100);
+  $("#login-box-link").addClass("active");
+  $("#signup-box-link").removeClass("active");
 });
