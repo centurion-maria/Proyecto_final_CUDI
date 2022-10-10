e3c8a7fcbca2b7cd30947379cdb0a6d8efbf6aee
Maria Centurion <centurionmalena94@gmail.com>
2022-10-10 12:33:19 -0300

    Navbar y search

--- js/componentes.js
+++ js/componentes.js
@@ -1,46 +1,45 @@
-class MyHeader extends HTMLElement{
-    connectedCallback() {
-        this.innerHTML =`
-            
-        <header class="header" id="header">
-            <section class="wrapper container">
-                <a href="index.html" class="brand">
-                <img src="Imagenes/logo_nav.png" alt="logo" width="40" height="35" class="d-inline-block align-text-top"> Planeta Mascotas 
-                </a>
-                <div class="burger" id="burger">
-                    <span class="burger-line"></span>
-                    <span class="burger-line"></span>
-                    <span class="burger-line"></span>
-                </div>
-                <span class="overlay"></span>
-                <nav class="navbar" id="navbar">
-                    <ul class="menu" id="menu">
-                        <li class="menu-item menu-dropdown">
-                            <span class="menu-link" data-toggle="submenu"><a href="productos.html" class="menu-link"> Productos <i class="bx bx-chevron-down"></a></i></span>
-                            <ul class="submenu">
-                                <li class="submenu-item"><a href="#" class="submenu-link"> Gatos </a></li>
-                                <li class="submenu-item"><a href="#" class="submenu-link"> Perros </a></li>
-                            </ul>
-                        </li>
-                        <li class="menu-item"><a href="login.html" class="menu-link"> Login </a></li>
-                        <li class="menu-item"><a href="acerca-de-nosotros.html" class="menu-link"> Acerca de nosotros </a></li>
-                        <li class="menu-item"><a href="servicios.html" class="menu-link"> Servicios </a></li>
-                        <li class="menu-item">
-                            <a href="carrito.html" class="menu-link">
-                                <img src="Imagenes/carrito_compras.png" alt="logo" width="35">
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </section>
-        </header>
-        `
-    }
-}
+/* Barra de navegación */
 
+class MyHeader extends HTMLElement{
+  connectedCallback() {
+    this.innerHTML =`
+    <div class="header">
+        <div class="navbar">
+          <div class="navbar__wrapper">
+            <nav class="navbar__menu">
+                <a href="index.html" class="brand">
+                    <img src="Imagenes/logo_nav.png" alt="logo" width="40" height="35" class="d-inline-block align-text-top"> Planeta Mascotas 
+                </a>
+              <div id="nav-icon3"><span></span><span></span><span></span><span></span></div>
+              <ul class="navbar__nav">
+                <li class="navbar__link after-transform"><a class="menu_link" href="productos.html">Productos</a></li>
+                <li class="navbar__link after-transform"><a class="menu_link" href="login.html">Login</a></li>
+                <li class="navbar__link after-transform"><a class="menu_link" href="acerca-de-nosotros.html">Acerca de Nosotros</a></li>
+                <li class="navbar__link after-transform"><a class="menu_link" href="servicios.html">Servicios</a></li>
+                <li class="navbar__link after-transform"><a class="menu_link" href="#">
+                    <img src="Imagenes/carrito_compras.png" alt="logo" width="35"></a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div class="navbar-responsive">
+          <ul class="navbar-responsive__nav">
+            <li class="navbar-responsive__link after-transform"><a href="productos.html">Productos</a></li>
+            <li class="navbar-responsive__link after-transform"><a href="login.html">Login</a></li>
+            <li class="navbar-responsive__link after-transform"><a href="acerca-de-nosotros.html">Acerca de nosotros</a></li>
+            <li class="navbar-responsive__link after-transform"><a href="servicios.html">Servicios</a></li>
+            <li class="navbar-responsive__link after-transform"><a href="#">Carrito</a></li>
+          </ul>
+        </div>
+      </div>
+    ` 
+  }
+}
 customElements.define('my-header', MyHeader)
 
 
+/* Footer */
 class MyFooter extends HTMLElement{
     connectedCallback() {
         this.innerHTML = `
@@ -57,3 +56,33 @@ class MyFooter extends HTMLElement{
     }
 }
 customElements.define('my-footer', MyFooter)
+
+
+/* Responsive de la pagina*/
+$(document).ready(function(){
+    var state = false,
+        links = $('.navbar-responsive__link')
+      $('#nav-icon3').click(function(){
+          $(this).toggleClass('open');
+      if(!state) {
+        $('.navbar-responsive').css("transform", "translate3d(0,0,0)")
+        state = true
+      } else {
+        $('.navbar-responsive').css("transform", "translate3d(-100%,0,0)")
+        state = false
+      }
+      
+      })
+    $.each(links, function(index,value){
+      value.addEventListener("click",function(){
+        if(!state) {
+          $('.navbar-responsive').css("transform", "translate3d(0,0,0)")
+          state = true
+        } else {
+          $('.navbar-responsive').css("transform", "translate3d(-100%,0,0)")
+          state = false
+        }
+        $('#nav-icon3').removeClass('open')
+      })
+    })
+  })
