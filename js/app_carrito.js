daa2798f4703dbad4118a0849641fc4160c1f4a2
Maria Centurion <centurionmalena94@gmail.com>
2022-11-04 21:17:59 -0300

    Update app_carrito.js

--- js/app_carrito.js
+++ js/app_carrito.js
@@ -22,24 +22,8 @@ const getStock = async () => {
     }
 };
 
-await axios.get('http://127.0.0.1:5000/productos')
-.then(response => {
-    const stockProductos = response.data;
-    console.log(stockProductos)
-    JSON.stringify(stockProductos)
-    console.log(stockProductos)
-})
-.catch(e => {
-    console.log(e);
-})
-
 const updateStockItems = stock => {
-    console.log(stock)
-
-
     const productos = stock
-
-    console.log(stock)
 
     productos.forEach((producto) => {
         const div = document.createElement('div')
@@ -56,11 +40,25 @@ const updateStockItems = stock => {
     
         boton.addEventListener('click', () => {
             agregarAlCarrito(producto.id)
-            //
         })
     })
-    console.log(productos)
+
+    const agregarAlCarrito = (prodId) => {
+        const existe = carrito.some (prod => prod.id === prodId)
+        if (existe){
+            const prod = carrito.map (prod => {
+                if (prod.id === prodId){
+                    prod.cantidad++
+                }
+            })
+        } else {
+            const item = stock.find((prod) => prod.id === prodId)
+            carrito.push(item)
+        }
+        actualizarCarrito()
+    }
 }
+
 
 const main = async () => {
     updateStockItems(await getStock());
@@ -81,27 +79,32 @@ botonVaciar.addEventListener('click', () => {
 })
 
 
-
-
-const agregarAlCarrito = (prodId) => {
-    const existe = carrito.some (prod => prod.id === prodId)
-    if (existe){
-        const prod = carrito.map (prod => {
-            if (prod.id === prodId){
-                prod.cantidad++
-            }
-        })
-    } else {
-        const item = stockProductos.find((prod) => prod.id === prodId)
-        carrito.push(item)
-    }
-    actualizarCarrito()
-}
-
 const eliminarDelCarrito = (prodId) => {
     const item = carrito.find((prod) => prod.id === prodId)
     const indice = carrito.indexOf(item)
     carrito.splice(indice, 1)
     actualizarCarrito() 
     console.log(carrito)
+}
+
+const actualizarCarrito = () => {
+    contenedorCarrito.innerHTML = ""
+    carrito.forEach((prod) => {
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio:$${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+        <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+        `
+        contenedorCarrito.appendChild(div)
+            
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+        
+    })
+    contadorCarrito.innerText = carrito.length
+    console.log(carrito)
+    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+
 }
