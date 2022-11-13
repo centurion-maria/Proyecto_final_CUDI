b824fd79a19817f1aaf90e8826033bb07ad40acc
Maria Centurion <centurionmalena94@gmail.com>
2022-11-13 20:02:22 -0300

    modal y form admin

--- js/app_form.js
+++ js/app_form.js
@@ -1,60 +1,3 @@
-/*  async function guardar() {
-    var data = {
-        "nombre": document.getElementById('nombre').value,
-        "cantidad": document.getElementById('cantidad').value,
-        "precio": document.getElementById('precio').value,
-        "imagen": document.getElementById('imagen').value.replace("C:\\fakepath\\", ""),
-    }
-    console.log(data)
-    var url = url + '/productos'
-    await fetch(url, {
-      "method": 'POST',
-      "body": JSON.stringify(data),
-      "headers": {
-        "Content-Type": 'application/json'
-      }
-    });
-
-    btn.addEventListener('click', (event) => {
-      event.preventDefault();
-      guardar();
-    })
-}*/
-
-/*axios.post("http://127.0.0.1:5000/productos", data, {
-  headers: {
-    "Content-Type": "multipart/form-data",
-  },
-});*/
-
-/*const formProducto = document.querySelector('#formProducto')
-
-formProducto.addEventListener('submit', e => {
-  e.preventDefault()
-
-  const nombre = formProducto['nombre'].value
-  const cantidad = formProducto['cantidad'].value
-  const precio = formProducto['precio'].value
-  const imagen = formProducto['imagen'].value.replace("C:\\fakepath\\", "")
-
-  fetch ('http://127.0.0.1:5000/productos', {
-    method: 'POST',
-    headers: {
-      "Content-Type": 'multipart/json'
-    },
-    body: JSON.stringify({
-      nombre,
-      cantidad,
-      precio,
-      imagen
-    })
-    .then(response => console.log(response))
-    .catch(error => console.log(error))
-  })
-  formProducto.reset()
-})*/
-
-
 const formProducto = document.querySelector('#formProducto')
 
 formProducto.addEventListener('submit', e => {
@@ -67,8 +10,6 @@ formProducto.addEventListener('submit', e => {
     imagen: document.getElementById('imagen').value.replace("C:\\fakepath\\", ""),
   };
 
-  console.log(data)
-
   const options = {method: 'POST', url: 'http://127.0.0.1:5000/productos', data: data};
 
   axios.request(options).then(function (response) {
@@ -76,5 +17,5 @@ formProducto.addEventListener('submit', e => {
   }).catch(function (error) {
     console.error(error);
   });
-
+  formProducto.reset()
 });
---
+++ js/modal2.js
@@ -0,0 +1,9 @@
+function agregarProducto(){
+    htmlModal = document.getElementById("modal2");
+    htmlModal.setAttribute("class", "modale opened");
+}
+
+function cerrarModale(){
+    htmlModal = document.getElementById("modal2");
+    htmlModal.setAttribute("class", "modale");
+}
